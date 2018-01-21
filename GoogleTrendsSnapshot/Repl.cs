30663ef8b2c7b0a16fda067de2bf6dec99b39b96
Nathan Hunt <nathan.taylor.hunt@gmail.com>
2018-01-21 18:01:28 -0500

    New and improved source code.

--- GoogleTrendsSnapshot/Repl.cs
+++ GoogleTrendsSnapshot/Repl.cs
@@ -1,24 +1,36 @@
 using System;
 using System.Text;
 using System.Linq;
-using System.Globalization;
 using System.Collections.Generic;
 
 namespace GoogleTrendsSnapshot
 {
+    /// <summary>
+    /// Read, print, eval, loop.
+    /// Command interface for the user.
+    /// </summary>
     public class Repl
     {
         private const string PROMPT_LINE = "> ";
+
+        /// <summary>
+        /// Commands that the user can use.
+        /// Note: Commands are uppercase
+        /// for easier string matching.
+        /// </summary>
         private enum Command
         {
-            Literal,
-            Exit,
-            Snap,
-            Delete,
-            Clear
+            LITERAL,
+            EXIT,
+            SNAP,
+            DELETE,
+            CLEAR
         }
 
-        public void Run()
+        /// <summary>
+        /// Start the repl, interpret user input.
+        /// </summary>
+        public void Run(Options options)
         {
             var searchTerms = new List<string>();
             while (true) {
@@ -32,26 +44,26 @@ namespace GoogleTrendsSnapshot
                 Console.WriteLine("Type \'exit\' to quit.");
                 Console.WriteLine("");
                 
-                var (nextTerm, termCommand) = ReadUserInput("ENTER A NEW SEARCH TERM:");
+                var prompt = "ENTER A NEW SEARCH TERM:";
+                var (nextTerm, termCommand) = ReadUserInput(prompt, options.AutoCapitalize);
                 switch (termCommand)
                 {
-                    case Command.Exit: return;
+                    case Command.EXIT: return;
                     
-                    case Command.Literal:
-                        if (searchTerms.Count < 5)
-                            searchTerms.Add(nextTerm);
+                    case Command.LITERAL:
+                        AddTerm(searchTerms, nextTerm);
                         break;
                     
-                    case Command.Delete:
+                    case Command.DELETE:
                         DeleteTerm(searchTerms, nextTerm);
                         break;
 
-                    case Command.Clear:
-                        searchTerms.Clear();
+                    case Command.CLEAR:
+                        ClearTerms(searchTerms);
                         break;
                     
-                    case Command.Snap:
-                        Snapshot.TakeSnapshot(searchTerms);
+                    case Command.SNAP:
+                        TakeSnapshot(searchTerms, options);
                         break;
                 }
             }
@@ -60,14 +72,16 @@ namespace GoogleTrendsSnapshot
         /// <summary>
         /// Read the user's input.  Check for 'exit' command.
         /// </summary>
-        /// <param name="prompt">Prompt message</param>
+        /// <param name="prompt">Prompt message.</param>
+        /// <param name="autoCap">Auto capitalize.</param>
         /// <returns>(User input, check for "exit" command)</returns>
-        private (string, Command) ReadUserInput(string prompt = "")
+        private (string, Command) ReadUserInput(string prompt = "", bool autoCap = true)
         {
             Console.WriteLine(prompt);
             Console.Write(PROMPT_LINE);
             
             var user_input = Console.ReadLine();
+            if (autoCap) user_input = user_input.ToUpper();
 
             return (user_input, ParseCommand(user_input));
         }
@@ -82,10 +96,9 @@ namespace GoogleTrendsSnapshot
         /// <returns>Matching Command enum.</returns>
         private Command ParseCommand(string input)
         {
-            Command result = Command.Literal;
+            Command result = Command.LITERAL;
             var firstToken = input.Split(" ")[0];
-            var correctedInput = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(firstToken);
-            Enum.TryParse(correctedInput, out result);
+            Enum.TryParse(firstToken.ToUpper(), out result);
             return result;
         }
 
@@ -114,6 +127,18 @@ namespace GoogleTrendsSnapshot
         }
 
         /// <summary>
+        /// Add new term to list. Limit to 5 max.
+        /// </summary>
+        /// <param name="terms">List of terms.</param>
+        /// <param name="newTerm">New term.</param>
+        private void AddTerm(List<string> terms, string newTerm)
+        {
+            int maxTerms = 5; // @Hardcoded, Google Trends only allows 5 terms max.
+            if (terms.Count < maxTerms)
+                terms.Add(newTerm);
+        }
+
+        /// <summary>
         /// Delete term from list by index.
         /// Parse second token of input as index,
         /// index starts at 1.
@@ -131,6 +156,32 @@ namespace GoogleTrendsSnapshot
                 if (index > 0 && index <= terms.Count)
                     terms.RemoveAt(index-1);
             }
+        }
+
+        /// <summary>
+        /// Clear all terms.
+        /// </summary>
+        /// <param name="terms">List of terms.</param>
+        private void ClearTerms(List<string> terms)
+        {
+            terms.Clear();
+        }
+
+        /// <summary>
+        /// Take a snapshot and report it's success and filename.
+        /// </summary>
+        /// <param name="terms">Search terms.</param>
+        /// <param name="options">Options.</param>
+        private void TakeSnapshot(List<string> terms, Options options)
+        {
+            Console.WriteLine("\nTAKING SNAPSHOT - PLEASE WAIT...\n");
+
+            var (filename, success) = Snapshot.TakeSnapshot(terms, options);
+            if (!success)   Console.WriteLine("\nERROR: Snapshot failed.");
+            else            Console.WriteLine("\nSNAPSHOT COMPLETE");
+
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE");
+            Console.ReadKey();
         }
     }
 }
--- GoogleTrendsSnapshot/Snapshot.cs
+++ GoogleTrendsSnapshot/Snapshot.cs
@@ -12,31 +12,58 @@ using OpenQA.Selenium.Interactions;
 
 namespace GoogleTrendsSnapshot
 {
-    public class Snapshot
+    /// <summary>
+    /// Use Selenium to take snapshot of Google Trends.
+    /// </summary>
+    public abstract class Snapshot
     {
         /// <summary>
         /// Given a list of terms, take a snapshot of the
         /// Google Trends line graph.
         /// </summary>
         /// <returns>(Filename, Success)</returns>
-        public static (string, bool) TakeSnapshot(List<string> searchTerms)
+        public static (string, bool) TakeSnapshot(List<string> searchTerms, Options options)
         {
-            // TODO: Load options from config file.
-            var options = new ChromeOptions();
-            options.AddArgument("--headless");
-            options.AddArgument("--window-size=1920,1080");
+            // Ensure chromedriver.exe can be found.
+            var chromeDriverDir = Path.GetFullPath(options.ChromeDriverDirectory);
+            if (!File.Exists(chromeDriverDir + "/chromedriver.exe"))
+            {
+                Console.WriteLine(">>> chromedriver.exe NOT FOUND");
+                Console.WriteLine(">>> " + chromeDriverDir);
+                return (null, false);
+            }
 
-            using (var driver = new ChromeDriver(@"./driver", options))
+            // Set ChromeDriver options.
+            var chromeOptions = new ChromeOptions();
+            if (options.Headless) chromeOptions.AddArgument("--headless");
+            chromeOptions.AddArgument("--window-size="+options.WindowSize);
+            chromeOptions.AddArgument("--log-level=3");
+            chromeOptions.AddArgument("--disable-extensions");
+
+            var service = ChromeDriverService.CreateDefaultService(chromeDriverDir);
+            service.SuppressInitialDiagnosticInformation = true;
+            service.EnableVerboseLogging = false;
+            service.HideCommandPromptWindow = true;
+
+            // Launch ChromeDriver
+            string filename;
+            using (var driver = new ChromeDriver(service, chromeOptions))
             {
                 try
                 {
+                    // 0. (Implicit) Open ChromeDriver.
+                    Console.WriteLine("> Opened ChromeDriver");
+
                     // 1. Navigate to Google Trends page.
-                    driver.Navigate().GoToUrl("https://trends.google.com/trends");
+                    driver.Navigate().GoToUrl(options.GoogleTrendsUrl);
+                    Console.WriteLine("> Navigated to {0}", options.GoogleTrendsUrl);
 
                     // 2. Enter list of terms into search bar.
                     var searchBar = driver.FindElementByCssSelector("search input");
-                    searchBar.SendKeys(String.Join(", ", searchTerms));
+                    var searchInput = String.Join(", ", searchTerms);
+                    searchBar.SendKeys(searchInput);
                     searchBar.SendKeys(Keys.Enter);
+                    Console.WriteLine("> Entered terms: {0}", searchInput);
                     
                     // 3. Setup snapshot (hover over most recent results).
                     var header = driver.FindElementByClassName("explorepage-content-header");
@@ -52,9 +79,13 @@ namespace GoogleTrendsSnapshot
                         (int) Math.Round(lineChart.Size.Width * 0.98f),
                         (int) Math.Round(lineChart.Size.Height / 2f));
                     hoverAction.Perform();
+                    Console.WriteLine("> Setup trends graph.");
 
                     // 4. Take snapshot.
                     var snapshot = (driver as ITakesScreenshot).GetScreenshot();
+                    Console.WriteLine("> Took snapshot.");
+
+                    // 5. Crop snapshot.
                     var rect = new Rectangle(
                         x: graphLocation.X,
                         y: headerLocation.Y,
@@ -63,20 +94,41 @@ namespace GoogleTrendsSnapshot
                     );
                     var originalSnapshot = Image.FromStream(new MemoryStream(snapshot.AsByteArray));
                     var croppedSnapshot = (originalSnapshot as Bitmap).Clone(rect, originalSnapshot.PixelFormat);
+                    Console.WriteLine("> Cropped snapshot.");
 
-                    // TODO: Determine actual file name.
-                    croppedSnapshot.Save("test_snapshot.png");
+                    // 5. Save file.
+                    filename = GetSnapshotFileName(options.SnapshotDirectory, "png");
+                    croppedSnapshot.Save(filename, ImageFormat.Png);
+                    Console.WriteLine("> Saved snapshot - {0}", filename);
                 }
-                catch
+                catch (Exception e)
                 {
-                    // TODO: Better handle error.
-                    Console.WriteLine("SOMETHING WENT WRONG");
-                    return ("", false);
+                    Console.WriteLine("\n>>> {0}\n", e.Message);
+                    return (null, false);
                 }
             }
 
-            // TODO: Return actual filename.
-            return ("result.png", true);
+            return (filename, true);
+        }
+
+        /// <summary>
+        /// Determine filename for snapshot.
+        /// Format:
+        /// 000-snapshot.EXT
+        /// Number is determined by total files
+        /// already in directory.
+        /// </summary>
+        /// <param name="directory">Snapshot directory.</param>
+        /// <param name="extension">File extension.</param>
+        /// <returns>Filename for snapshot.</returns>
+        public static string GetSnapshotFileName(string directory, string extension)
+        {
+            var files = Directory.GetFiles(directory, "*", SearchOption.TopDirectoryOnly);
+            var fileCount = files.Length + 1;
+            return string.Format("{0}/{1}_snapshot.{2}",
+                    directory,
+                    fileCount.ToString().PadLeft(3, '0'),
+                    extension);
         }
     }
 }
